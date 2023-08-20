d4848b254ed4133dfd94e53ee6cd3561449ac7a8
owobred <29340449+owobred@users.noreply.github.com>
2023-08-20 08:33:26 +0100

    initial commit :3

---
+++ injector/src/main.rs
@@ -0,0 +1,93 @@
+use std::sync::{Arc, Mutex};
+
+use dll_syringe::{process::OwnedProcess, Syringe};
+use once_cell::sync::Lazy;
+use windows::Win32::{
+    Foundation::{BOOL, HWND, LPARAM},
+    System::Threading,
+    UI::WindowsAndMessaging,
+};
+
+static OUTPUT_VEC: Lazy<Mutex<Vec<(u32, u32, HWND, String)>>> = Lazy::new(|| Mutex::new(vec![]));
+
+fn main() {
+    unsafe {
+        WindowsAndMessaging::EnumWindows(Some(window_iter), LPARAM::default())
+            .expect("failed to iterate over windows");
+    }
+
+    let output = std::mem::take(&mut (*OUTPUT_VEC.lock().unwrap()));
+    let output = output.into_iter().filter(|info| info.3 == "Spotify Free").collect::<Vec<_>>();
+
+    println!("{output:?}");
+
+    let processes = output
+        .iter()
+        .map(|(pid, window_thread_pid, hwnd, name)| OwnedProcess::from_pid(*pid).unwrap());
+
+    let mut syringes = vec![];
+    for process in processes {
+        let syringe = Syringe::for_process(process);
+        syringes.push(syringe);
+    }
+
+    let mut modules = vec![];
+    for (syringe, info) in syringes.iter().zip(output) {
+        let module = syringe.inject("target\\debug\\to_inject.dll");
+        modules.push((module, info));
+    }
+
+    let to_eject =  syringes.iter().zip(modules).filter_map(|(syringe, (result, (pid, thread_pid, hwnd, name)))| {
+        match result {
+            Ok(module) => Some((syringe, (module, (pid, thread_pid, hwnd, name)))),
+            Err(error) => {
+                println!("failed to inject into {name} (pid {pid}): {error:?}");
+                None
+            },
+        }
+    }).collect::<Vec<_>>();
+
+    for (syringe, (module, info)) in to_eject {
+        syringe.eject(module).expect("failed to eject");
+        println!("ejected {info:?}")
+    }
+
+    // let processes = OwnedProcess::find_all_by_name("Spotify");
+
+    // let mut syringes = vec![];
+    // for process in processes {
+    //     let syringe = Syringe::for_process(process);
+    //     syringes.push(syringe);
+    // }
+
+    // let mut modules = vec![];
+    // for syringe in &syringes {
+    //     let module = syringe.inject("target\\debug\\to_inject.dll").map_err(|_| eprintln!("failed to inject into {:?}", syringe.process()));
+    //     if let Ok(module) = module {
+    //         println!("injected into {:?}", syringe.process());
+    //         modules.push((syringe, module));
+    //     }
+    // }
+
+    // std::thread::sleep(std::time::Duration::from_secs(4));
+
+    // for (syringe, module) in modules {
+    //     syringe.eject(module).expect("failed to eject");
+    // }
+}
+
+unsafe extern "system" fn window_iter(hwnd: HWND, _lparam: LPARAM) -> BOOL {
+    let mut pid: u32 = 0;
+    let window_thread_pid =
+        WindowsAndMessaging::GetWindowThreadProcessId(hwnd, Some(&mut pid as *mut u32));
+
+    let mut window_name = [0u8; 1024];
+    let window_name_len = WindowsAndMessaging::GetWindowTextA(hwnd, &mut window_name);
+    let window_name = &window_name[..window_name_len as usize];
+    let name = String::from_utf8_lossy(window_name);
+
+    let mut lock = OUTPUT_VEC.lock().unwrap();
+    lock.push((pid, window_thread_pid, hwnd, name.to_string()));
+
+    return true.into();
+}
---
+++ to-inject/src/lib.rs
@@ -0,0 +1,87 @@
+use std::{
+    ffi::{CStr, CString, OsString},
+    os::windows::prelude::OsStringExt,
+    sync::Mutex,
+};
+
+use once_cell::sync::Lazy;
+use windows::{
+    core::{s, HSTRING, PCSTR},
+    Win32::{
+        Foundation::*,
+        System::{Environment::GetCommandLineA, SystemServices::*, Threading},
+        UI::WindowsAndMessaging::{self, MessageBoxA},
+    },
+};
+
+#[no_mangle]
+extern "system" fn DllMain(_dll_module: HINSTANCE, call_reason: u32, _: *mut ()) -> bool {
+    match call_reason {
+        DLL_PROCESS_ATTACH => attach(),
+        DLL_PROCESS_DETACH => detach(),
+        _ => (),
+    }
+
+    true
+}
+
+fn attach() {
+    unsafe {
+        let our_hwnds = get_whwnds();
+        // let text = CString::new(format!("{our_hwnds:?}").as_bytes().to_vec()).unwrap();
+        // MessageBoxA(
+        //     HWND(0),
+        //     PCSTR(text.as_ptr() as *const u8),
+        //     s!("inject 1"),
+        //     Default::default(),
+        // );
+        for hwnd in our_hwnds {
+            WindowsAndMessaging::SetWindowDisplayAffinity(
+                hwnd,
+                WindowsAndMessaging::WDA_EXCLUDEFROMCAPTURE,
+            ).map_err(|error| {
+                let text = CString::new(format!("{error:?}").as_bytes().to_vec()).unwrap();
+
+
+                MessageBoxA(HWND(0), PCSTR(text.as_ptr() as *const u8), s!("error excluding"), Default::default())
+            }).ok();
+        }
+    }
+}
+
+fn detach() {
+    // unsafe {
+    // MessageBoxA(
+    //     HWND(0),
+    //     s!("uninject 1"),
+    //     s!("uninject 2"),
+    //     Default::default(),
+    // );
+    // }
+}
+
+static OUTPUT_VEC: Lazy<Mutex<Vec<(u32, HWND)>>> = Lazy::new(|| Mutex::new(vec![]));
+
+unsafe fn get_whwnds() -> Vec<HWND> {
+    #[cfg(target_pointer_width = "32")]
+    {
+        compile_error!("this is 100% gonna be broken on 32 bit");
+    }
+
+    let our_pid = Threading::GetCurrentProcessId();
+    WindowsAndMessaging::EnumWindows(Some(window_iter), LPARAM(our_pid as isize))
+        .expect("failed to iter windows");
+    let output = std::mem::take(&mut (*OUTPUT_VEC.lock().unwrap()));
+    return output.into_iter().map(|(_, hwnd)| hwnd).collect();
+}
+
+unsafe extern "system" fn window_iter(hwnd: HWND, lparam: LPARAM) -> BOOL {
+    let mut pid: u32 = 0;
+    WindowsAndMessaging::GetWindowThreadProcessId(hwnd, Some(&mut pid as *mut u32));
+    if pid == lparam.0 as u32 {
+        let mut lock = OUTPUT_VEC.lock().expect("failed to unwrap vec");
+        lock.push((pid, hwnd));
+    }
+
+    return true.into();
+}
